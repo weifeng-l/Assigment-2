71d53a5e2960e1ad72d592b0e23008b72df7ff88
weifeng-l <s10208179@connect.np.edu.sg>
2021-01-10 17:09:44 +0800

    added wind variable

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var temp = document.querySelector('.temp');
 var desc = document.querySelector('.description');
 var clouds = document.querySelector('.clouds');
 var button= document.querySelector('.submit');
+var wind = document.querySelector('.wind');
 
 
 
@@ -12,15 +13,17 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=a
 
 .then(response => response.json())
 .then(data => {
-  var tempValue = (data.main.temp - 273.15).toFixed(2) + ' °C';
+  var tempValue = (data.main.temp - 273.15).toFixed(2);
   var nameValue = data.name;
   var descValue = data.weather[0].description;
   var cloudsValue = data.clouds.all;
+  var windValue = (data.wind.speed * 2.237).toFixed(2);
 
   main.innerHTML = nameValue;
   desc.innerHTML = "Weather Condition - "+descValue;
-  temp.innerHTML = "Temp - "+tempValue;
+  temp.innerHTML = "Temp - "+tempValue  + ' °C';
   clouds.innerHTML = "clouds - "+ cloudsValue + "%";
+  wind.innerHTML = "Wind speed - " + windValue + "mph"
   input.value ="";
 
 })
